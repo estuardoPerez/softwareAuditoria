7c58ea07727555231f6e4a9c5bab650483f5e5b1
karlacq <karlacruz795@gmail.com>
2019-10-16 09:50:10 -0600

    ACTUALIZACIÓN

---
+++ karlaModulo/karlaModulo/karlaModulo/mantenimientoAuditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace karlaModulo
+{
+    public partial class mantenimientoAuditor : Form
+    {
+        public mantenimientoAuditor()
+        {
+            InitializeComponent();
+        }
+    }
+}

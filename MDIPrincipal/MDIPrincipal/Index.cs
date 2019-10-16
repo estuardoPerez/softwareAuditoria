30c24eda5892a54d20d6d16e9d4d2e0151bce175
Julio Sicaja <elmasterrr000@gmail.com>
2019-10-15 23:02:05 -0600

    MDI

---
+++ MDIPrincipal/MDIPrincipal/Index.cs
@@ -0,0 +1,80 @@
+﻿/* AUTOR: JULIO SICAJA */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ejemploVentana;
+
+namespace MDIPrincipal
+{
+    public partial class Index : Form
+    {
+        private void priChildMDI(Form fmr)
+        {
+           if (Application.OpenForms[fmr.Name] != null)
+            {
+                Application.OpenForms[fmr.Name].Activate();
+            }
+            else
+            {
+                fmr.MdiParent = this;
+                fmr.StartPosition = FormStartPosition.CenterScreen;
+                fmr.Show();
+            }
+        }
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(Screen.PrimaryScreen.WorkingArea.Size.Width, Screen.PrimaryScreen.WorkingArea.Size.Height);
+            this.Location = new Point(0, 0);  //sobra si tienes la posición en el diseño
+            Form fmr_logo = new logo();
+            fmr_logo.MdiParent = this;
+            fmr_logo.StartPosition = FormStartPosition.CenterScreen;
+            fmr_logo.Enabled = false;
+            fmr_logo.Show();
+
+            MdiClient ctlMDI;
+            foreach (Control ctl in this.Controls)
+            {
+                try
+                {
+                    ctlMDI = (MdiClient)ctl;
+                    ctlMDI.BackColor = this.BackColor;                
+                }
+                catch (InvalidCastException exc)
+                {
+
+                }
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void HolaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            priChildMDI(new Form1());
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MDIPrincipal/MDIPrincipal/logo.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace MDIPrincipal
+{
+    partial class logo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // logo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MDIPrincipal.Properties.Resources.Logo_Auditoria;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(396, 396);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "logo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "logo";
+            this.LocationChanged += new System.EventHandler(this.Logo_LocationChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
